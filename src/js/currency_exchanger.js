0c20b68e708491200b240b3ed9851b2f92c09f0c
Ben Stoller <stollersystemsound@gmail.com>
2020-09-11 09:48:42 -0700

    test error handling if failing to fetch

--- src/js/currency_exchanger.js
+++ src/js/currency_exchanger.js
@@ -1,8 +1,8 @@
 export default class CurrencyExchange {
   static getRates(currency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)    
       .then(function(response){
-        console.log(response.statusText)
+        //console.log(response.statusText)
         if (!response.ok) {
           // console.log(response.statusText)
           throw Error (response.statusText);
