e073382d96947637d257b6ea43017caddb7bc700
Ben Stoller <stollersystemsound@gmail.com>
2020-09-11 08:33:17 -0700

    build out initial UI logic

--- src/main.js
+++ src/main.js
@@ -2,3 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './js/currency_exchanger.js'
+
+
+
+$(document).ready(function() {
+  $('#getRates').click(function() {
+
+    let city = $('#location').val();    
+
+    CurrencyExchange.getRates()
+      .then(function(response) {
+        getElements(response);
+      });
+  });
+});
+
+function getElements (response) {
+  if (response) {
+
+  } else {
+    $("").text(`There was an error: ${response.message}`)
+  }
+
+}
