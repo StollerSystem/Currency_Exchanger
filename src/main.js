9c6f51cf5ff01906351108363175a0cc002a6620
Ben Stoller <stollersystemsound@gmail.com>
2020-09-12 11:52:29 -0700

    refactor country_info js

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './js/currency_exchanger.js';
-import countryArray from './js/country_info.js';
+import CountryInfo from './js/country_info.js';
 
 class Storage {
   constructor() {
-    this.results = [];
+    this.results = [];    
   }
 }
 
-let StoreResponse = new Storage();
+let countryArray = new CountryInfo();
+let storeResponse = new Storage();
 
 $(document).ready(function() {
   attachListeners();
@@ -31,8 +32,8 @@ $(document).ready(function() {
 function getElements (response) {
   if (response.result === "success") {    
     $("#date").html(`(Rates last updated ${response.time_last_update_utc.slice(0,25)})`);
-    StoreResponse.results = Object.entries(response.conversion_rates);    
-    StoreResponse.results.forEach(function(rate){
+    storeResponse.results = Object.entries(response.conversion_rates);    
+    storeResponse.results.forEach(function(rate){
       $("#output").append(`<li class="rateItem" id="${rate[0]}"><span class="code">${rate[0]}</span>: <span class="rate">${rate[1]}</span></li>`);
     });    
   } else if (response.result === "error") {
@@ -47,15 +48,21 @@ function attachListeners() {
   $("ul#output").on("click",".rateItem", function(){        
     let id = this.id;
     let countryInfo = "";
-    for (const element of countryArray) {
+    for (const element of countryArray.countries) {
       if (element.includes(id)) {       
         countryInfo = element.split("-");
         break;
       }
     }    
-    StoreResponse.results.forEach(function(code){      
+    storeResponse.results.forEach(function(code){      
       if (id === code[0]) {          
-        $("#details").html(`<p><img src="https://www.countryflags.io/${getFlagCode()}/shiny/64.png">  <span class="detMid">${StoreResponse.results[0][1]}</span> <span class="detLeft">${StoreResponse.results[0][0]}</span> = <span class="detMid">${code[1]} </span><span class="detCount">${countryInfo[1]}</span> (${countryInfo[2]})  <img src="https://www.countryflags.io/${countryInfo[3]}/shiny/64.png"></p`);
+        $("#details").html(`
+        <p><img src="https://www.countryflags.io/${getFlagCode()}/shiny/64.png">  
+        <span class="detMid">${storeResponse.results[0][1]}</span> 
+        <span class="detLeft">${storeResponse.results[0][0]}</span> = 
+        <span class="detMid">${code[1]} </span>
+        <span class="detCount">${countryInfo[1]}</span> (${countryInfo[2]})  
+        <img src="https://www.countryflags.io/${countryInfo[3]}/shiny/64.png"></p`);
       }
     });
     $("#details p").show();
@@ -63,8 +70,8 @@ function attachListeners() {
 }
 
 function getFlagCode() {  
-  for (const element of countryArray) {
-    if (element.includes(StoreResponse.results[0][0])) {             
+  for (const element of countryArray.countries) {
+    if (element.includes(storeResponse.results[0][0])) {             
       return element.split("-")[3];
     }
   }
